899d5e2fc33b50cd177ecf0274c248ea661f5772
Roy Jiny <48204245+RoyJiny@users.noreply.github.com>
2021-11-18 22:21:15 +0200

    temp commit

--- backend/src/routers/users_router.js
+++ backend/src/routers/users_router.js
@@ -3,9 +3,20 @@ const router = new express.Router();
 
 const auth = require('../middleware/auth');
 
+router.post("/users/register", (req,res) => {
+    user_id = req.body.user_id;
+    if (!user_id) {
+        console.log('No user id provided');
+        res.status(401).send('Wrong data provided');
+        return;
+    }
+    console.log(`Received user id '${user_id}''`)
 
-router.get("/users/test", auth, (req,res) => {
-    res.send('Hi there');
+    /* get token from firebase by firebase uid */
+    
+    /* save token inside mongo db (and create a new user) */
+
+    res.send('User registered successfuly');
 });
 
 module.exports = router;
