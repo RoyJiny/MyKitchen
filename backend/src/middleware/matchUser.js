470d18f76214c6b0d2f4ad22d0e8c1b1ced05784
Ori Fisher <orifisher@mail.tau.ac.il>
2021-11-30 20:16:17 +0200

    match kitchen user added for dvir

--- backend/src/middleware/matchUser.js
+++ backend/src/middleware/matchUser.js
@@ -37,3 +37,23 @@ const matchUserOrder = async (req, res, next) => {
 };
 
 module.exports = matchUserOrder;
+
+const matchKitchenOrder = async (req, res, next) => {
+    try {
+        orderID = req.body.id;
+        kitchenID = req.user.kitchen;
+
+        const order = await Order.findById(orderID);
+
+        if (!order.kitchen.equals(kitchenID)) {
+            throw new Error();
+        }
+
+        req.order = order;
+        next();
+    } catch (e) {
+        res.status(401).send({ error: "User auth not matching Order" });
+    }
+};
+
+module.exports = matchKitchenOrder;
