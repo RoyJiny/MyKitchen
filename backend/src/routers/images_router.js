e66d3ea868acce15b4bbca498ef7877561e15fa6
Roy Jiny <royjiny@gmail.com>
2021-12-06 22:01:56 +0200

    Add SMS Verification

--- backend/src/routers/images_router.js
+++ backend/src/routers/images_router.js
@@ -14,41 +14,41 @@ const multer_upload = multer({dest: './uploads/'});
 // TODO: also add matchUserKitchen function to verify the user
 router.post("/upload/", auth, multer_upload.single('img'), async (req,res) => {
     try {
-        const file_path = './uploads/' + req.file.filename;
+      const file_path = './uploads/' + req.file.filename;
         
-        const image_obj = {
-          img: {
-            data: fs.readFileSync(file_path),
-            contentType: 'image/jpg'
-          }
-        };
+      const image_obj = {
+        img: {
+          data: fs.readFileSync(file_path),
+          contentType: 'image/jpg'
+        }
+      };
 
-        const image = new Image(image_obj);
-        await image.save();
-        fs.unlinkSync(file_path); // delete local file after upload to the db
-        
-        const kitchenId = req.body.kitchenId;
-        const kitchen = await Kitchen.findById(kitchenId);
-        if (!kitchen) throw new Error();
-        
-        switch (req.body.type) {
-          case 'coverImg':    
-            kitchen.bio.coverImg = image._id;
-            await kitchen.save();
-            break;
-          case 'dishImg':
-            const dishIdx = kitchen.menu.findIndex(dish => dish._id.toString() === req.body.dishId);
-            kitchen.menu[dishIdx].img = image._id;
-            await kitchen.save();
-            break;
-          default:
-            throw new Error('Unknown image type');
-        };
+      const image = new Image(image_obj);
+      await image.save();
+      fs.unlinkSync(file_path); // delete local file after upload to the db
+      
+      const kitchenId = req.body.kitchenId;
+      const kitchen = await Kitchen.findById(kitchenId);
+      if (!kitchen) throw new Error();
+      
+      switch (req.body.type) {
+        case 'coverImg':    
+          kitchen.bio.coverImg = image._id;
+          await kitchen.save();
+          break;
+        case 'dishImg':
+          const dishIdx = kitchen.menu.findIndex(dish => dish._id.toString() === req.body.dishId);
+          kitchen.menu[dishIdx].img = image._id;
+          await kitchen.save();
+          break;
+        default:
+          throw new Error('Unknown image type');
+      };
 
-        res.sendStatus(200);
+      res.sendStatus(200);
     } catch (err) {
-        console.log(err);
-        res.status(500).send('Server Error');
+      console.log(err);
+      res.status(500).send('Server Error');
     }
 });
 
