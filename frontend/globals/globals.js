90ddf5fb7ee4c6171eb87693e7110e4059eaf104
Ori Fisher <orifisher@mail.tau.ac.il>
2022-01-02 01:01:12 +0200

    favorites now updating on context correctly

--- frontend/globals/globals.js
+++ frontend/globals/globals.js
@@ -1,2 +1,2 @@
-// export const ServerBase = "https://workshop-6b826.ew.r.appspot.com";
-export const ServerBase = "https://99f8-80-230-73-20.ngrok.io";
+export const ServerBase = "https://workshop-6b826.ew.r.appspot.com";
+//export const ServerBase = "https://99f8-80-230-73-20.ngrok.io";
