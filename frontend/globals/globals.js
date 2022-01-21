6831df9f1597fae10c5a74c3e21c6343752ff868
Roy Jiny <royjiny@gmail.com>
2022-01-21 11:08:50 +0200

    change server address back

--- frontend/globals/globals.js
+++ frontend/globals/globals.js
@@ -1,2 +1,2 @@
-// export const ServerBase = "https://workshop-6b826.ew.r.appspot.com";
-export const ServerBase = "https://0dec-176-230-237-157.ngrok.io";
+export const ServerBase = "https://workshop-6b826.ew.r.appspot.com";
+// export const ServerBase = "https://0dec-176-230-237-157.ngrok.io";
